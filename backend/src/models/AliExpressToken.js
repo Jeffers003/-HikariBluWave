9c0ae0f0548a4372bdf39e8165c36ca0555bb0d4
jeff <jeffinhosilva878@gmail.com>
2026-08-07 18:36:12 -0300

    ateração de api

--- backend/src/models/AliExpressToken.js
+++ backend/src/models/AliExpressToken.js
@@ -1,10 +1,5 @@
 // src/services/aliexpressToken.js
-//
-// Extraído de aliexpressService.js para quebrar a dependência circular:
-// antes, aliexpressService.js <-> aliexpressClient.js se importavam
-// mutuamente. Agora os dois importam apenas daqui.
-
-import AliExpressToken from "./aliexpressToken.js";
+import AliExpressToken from "../models/AliExpressToken.js";
 
 export async function obterAccessToken() {
   const token = await AliExpressToken.findOne();
@@ -12,11 +7,6 @@ export async function obterAccessToken() {
   if (!token) {
     throw new Error("Nenhum token do AliExpress encontrado.");
   }
-
-  // TODO: checar token.expireTime aqui e renovar automaticamente
-  // com refresh_token quando estiver perto de expirar. Por enquanto
-  // isso não existe e a chamada vai falhar com token expirado depois
-  // do tempo de expires_in.
 
   return token.accessToken;
 }
